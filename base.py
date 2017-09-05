0f59bc46aa3dcdf6faf096cd8a0f4d319b1dbf45
juxiaona <juxiaona@xxkuaipao.com>
2017-09-05 09:45:18 +0800

    'rename

---
+++ base.py
@@ -0,0 +1,29 @@
+from time import sleep
+
+class BasePage():
+
+	def __init__(self,driver):
+		self.driver=driver
+
+	def get_element(self,*loc):
+		return self.driver.find_element(*loc)
+
+	def wait_element(self,by,element):
+		for i in range(30):
+			print(i)
+			try:
+				element=self.driver.find_element(by,element)
+				if element.is_displayed():
+					break;
+			except:
+				pass
+			sleep(1)
+
+
+	def switch_frame(self,element):
+		self.driver.switch_to.frame(element)
+
+
+	def switch_default(self):
+		self.driver.switch_to.default_content()
+
